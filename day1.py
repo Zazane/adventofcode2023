7b42b931641523fc6b4f6195dbe49ea9b7e330f8
Zane <zanenor@gmail.com>
2023-12-05 20:48:22 +0200

    changed approach based on second part of task

--- day1.py
+++ day1.py
@@ -1,22 +1,51 @@
 # On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number
 # Have to add them all together to get final answer
+# Part 2: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits"
+# Written digits can be blended together like eightwo = 82
 
 answer = 0
-file = open(r"day1_input.txt", "r")
+file = open("day1_input.txt", "r")
+
+written = {
+  "one": '1',
+  "two": '2',
+  "three": '3',
+  "four": '4',
+  "five": '5',
+  "six": '6',
+  "seven": '7',
+  "eight": '8',
+  "nine": '9'
+}
 
 for line in file:
-    print(line)
     number = 0
-    numbers = "".join([num for num in line if num.isdigit()]) # Keep digits
-    if (len(numbers) > 2): # First and last
-        number = numbers[0] + numbers[-1]
-    elif (len(numbers) == 1): # If only one then have it twice
-        number = numbers[0] + numbers[0]
-    elif (len(numbers) == 2): # If two then keep as is
-        number = numbers
-    if (len(numbers) > 0):
-        answer = answer + int(number)
-    print(number)
+    line.lower()
+
+    first = 0
+    last = 0
+    write = ''
+    for ele in line:
+        if (ele.isdigit() and first == 0):
+            first = ele
+        elif (ele.isdigit()):
+            last = ele
+            write = ''
+        else:
+            write = write + ele
+            if (len(write) > 2):
+                for key in written:
+                    if (key in write):
+                        if (first == 0):
+                            first = written[key]
+                            write = key[1:]
+                        else:
+                            last = written[key]
+                            write = key[1:]
+
+    if (last == 0):
+        last = first
+    answer = answer + int(str(first) + str(last))
 
 print(answer)
 file.close()
